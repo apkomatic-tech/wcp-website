e0a4f0a555e21eae7a0adf6dfa75fe7bec4cfc43
Konstantin M <humbl3dev@gmail.com>
2020-09-27 15:44:09 -0700

    wrap page elmeent

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,7 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+// https://www.gatsbyjs.org/docs/browser-apis/
+import React from "react"
+import Layout from "./src/components/layout"
 
-// You can delete this file if you're not using it
+export function wrapPageElement({ element, props }) {
+  return <Layout {...props}>{element}</Layout>
+}
