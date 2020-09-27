572efaab8fa6d06a8fb408d72a9c47bdff27ba1e
Konstantin M <humbl3dev@gmail.com>
2020-09-27 15:44:15 -0700

    add global styles

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,28 @@
+export default `
+
+    :root {
+        --darkGrey: #2B2D42;
+        --lightGrey: #8D99AE;
+        --offWhite: #EDF2F4;
+        --red: #EF233C;
+        --darkRed: #9a031e;
+    }
+
+    html {
+        font-size: 10px;
+    }
+
+    body {
+        font-size: 1.8rem;
+        font-family: Montserrat, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+        line-height: 1.4;
+        background-color: var(--offWhite);
+    }
+
+    .text-center {
+        text-align: center;
+    }
+    .text-right {
+        text-align: right;
+    }
+`
