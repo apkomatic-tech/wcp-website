6881ce472e37998aeaf10d4add3b190b75efc22a
Konstantin M <humbl3dev@gmail.com>
2020-11-25 11:45:01 -0800

    add careers, programs, training pages (placeholder content)

---
+++ src/pages/careers.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+import SEO from "../components/seo"
+
+export default () => {
+  return (
+    <>
+      <SEO title="Careers" />
+      <h1>Careers</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+        alias, quo culpa suscipit neque perferendis at nihil omnis earum velit
+        asperiores. Aspernatur nam quidem odit quibusdam, natus, nihil quo
+        laudantium harum deleniti nulla eum saepe debitis quos adipisci vel
+        distinctio aliquam ipsa omnis doloremque blanditiis quaerat ut expedita
+        mollitia magni. Doloremque voluptatem voluptates deleniti dignissimos
+        obcaecati id sit, ipsum quas at laborum quo doloribus quisquam omnis
+        alias rem, recusandae itaque. Expedita cumque libero maxime vel animi
+        sint ea porro nostrum, ab harum a? Dolor tempora molestias distinctio
+        harum, iure sequi eligendi, similique rerum eaque expedita dignissimos
+        cupiditate. Earum, expedita reprehenderit.
+      </p>
+    </>
+  )
+}
---
+++ src/pages/programs/index.js
@@ -0,0 +1,49 @@
+import React from "react"
+import styled from "styled-components"
+
+import SEO from "../../components/seo"
+
+export default () => {
+  return (
+    <>
+      <SEO title="Programs" />
+      <h1>Programs</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore placeat
+        aliquam doloremque aut eligendi dolor voluptatum iure, quisquam illo!
+        Molestiae hic, ipsa sequi nesciunt aliquid cum harum obcaecati et
+        impedit incidunt. Nulla ipsum consectetur quae quisquam incidunt sunt
+        eaque numquam expedita repellendus laboriosam, debitis dolor rem
+        corrupti nihil minus eum fuga quia hic in. Quisquam temporibus libero
+        dignissimos quidem repellendus, consectetur necessitatibus voluptatibus
+        quam enim nesciunt magnam quis labore sint excepturi doloremque illo
+        dolor nisi, aliquid dolorum iure sunt, consequatur delectus? Ea eligendi
+        minima necessitatibus, saepe nulla, exercitationem consequatur fuga sit
+        dolor impedit recusandae error et ipsum esse quibusdam magnam tempora
+        dignissimos, temporibus tempore! Pariatur commodi rem eaque dolores
+        expedita quia totam animi dolorum. Illo sequi molestiae impedit
+        praesentium consequatur nam asperiores rerum itaque minima sapiente sit
+        magni corrupti pariatur voluptatum voluptatibus dicta velit ad,
+        perspiciatis dolorem quia voluptatem quibusdam et provident? Sequi minus
+        voluptas molestiae, fugit libero explicabo expedita, beatae doloribus
+        nihil omnis atque nobis perferendis consequatur, officia repellat!
+        Pariatur animi labore facere quod esse molestiae neque sequi delectus
+        unde necessitatibus, quos obcaecati voluptatum, assumenda sapiente. Eius
+        ut ipsam doloribus! Voluptatibus dicta nemo ut tempore exercitationem
+        eos libero. Odio quibusdam neque hic doloremque quia cum a ad deserunt,
+        placeat aliquam corrupti animi quos beatae optio eaque quam fugit
+        voluptate, voluptas mollitia rerum nobis explicabo eligendi iure quis?
+        Soluta, voluptate ducimus amet esse nostrum alias error autem, corporis
+        assumenda eveniet debitis, ut adipisci! Dicta sed perferendis fugit
+        consequatur, ratione sunt rerum, voluptatem sapiente molestiae magnam
+        delectus alias minus, placeat qui. Ut possimus quia, ab fugit, ad animi
+        corrupti, quisquam itaque ipsum explicabo similique. Fugiat, laborum
+        omnis? Labore sit suscipit magnam, voluptate quas provident sapiente
+        aperiam autem itaque, veritatis recusandae tempora. Porro temporibus
+        deserunt rem cum itaque. Quos fuga repellendus incidunt expedita
+        asperiores dolores, vero nulla. Corrupti recusandae aliquid id?
+        Doloremque.
+      </p>
+    </>
+  )
+}
