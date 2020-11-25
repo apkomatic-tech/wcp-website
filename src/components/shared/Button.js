7b3284f46ac144d9355f71ee74686c24e924ffec
Konstantin M <humbl3dev@gmail.com>
2020-11-25 11:44:20 -0800

    update button styling

--- src/components/shared/Button.js
+++ src/components/shared/Button.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 
 const Button = styled.button`
   appearance: none;
-  border-radius: 5px;
+  border-radius: 3px;
   border: 0;
   box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
   cursor: pointer;
@@ -14,11 +14,27 @@ const Button = styled.button`
   transition: 100ms ease;
   background-color: ${(props) =>
     props.primary ? "var(--primary)" : "var(--secondary)"};
-  color: ${(props) => (props.primary ? "var(--white)" : "var(--black)")};
+  color: ${(props) => (props.primary ? "var(--white)" : "var(--white)")};
+  box-shadow: 5px 5px 0 rgba(0, 0, 0, 0.5);
+  transition: 300ms ease;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  svg {
+    margin-left: 0.5rem;
+    font-size: 120%;
+    transform: translateY(2px);
+    transition: 100ms ease;
+  }
   &:hover,
   &:active,
   &:focus {
-    color: ${(props) => (props.primary ? "var(--white)" : "var(--black)")};
+    color: ${(props) => (props.primary ? "var(--white)" : "var(--white)")};
+    box-shadow: 3px 3px 0 rgba(0, 0, 0, 0.5);
+  }
+  &:hover svg,
+  &:focus svg {
+    transform: translateY(2px) translateX(3px);
   }
 `
 const ButtonLink = Button.withComponent(Link)
